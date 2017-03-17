c19cc68186367d603a80e666df325a67b8b93883
Kent <romeobidien@gmail.com>
2017-03-17 22:07:49 +0700

    Chapter 24 : More about Intents and Tasks : NerdLauncher       initial setup

---
+++ app/src/main/java/com/canation/android/nerdlauncher/NerdLauncherFragment.java
@@ -0,0 +1,32 @@
+package com.canation.android.nerdlauncher;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by CangNguyen on 3/17/2017.
+ */
+
+public class NerdLauncherFragment extends Fragment {
+    private RecyclerView mRecyclerView;
+
+    public static NerdLauncherFragment newInstance() {
+        return new NerdLauncherFragment();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_nerd_launcher, container, false);
+        mRecyclerView = (RecyclerView) v.findViewById(R.id.app_recycler_view);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        return v;
+    }
+}
